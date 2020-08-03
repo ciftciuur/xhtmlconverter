c9b7a660a7c12bb53416572a00022ef7d746ea6c
ugur ciftci <ugurprog@gmail.com>
2020-08-03 20:03:04 +0300

    created converter class

---
+++ converter/src/RichToPrime.java
@@ -0,0 +1,133 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RichToPrime {
+    private String rihc, prime;
+    static List<RichToPrime> list = new ArrayList<>();
+
+    static {
+
+        list.add(new RichToPrime("noSelectionLabel=\"SEÇİNİZ.\"", ""));
+
+        list.add(new RichToPrime("onclick=\"Richfaces.showModalPanel('LoadingPanel')\"", ""));
+        list.add(new RichToPrime("Richfaces.showModalPanel('LoadingPanel');", ""));
+        list.add(new RichToPrime("Richfaces.hideModalPanel('LoadingPanel');", ""));
+
+        list.add(new RichToPrime("</a4j:region>", ""));
+        list.add(new RichToPrime("<a4j:region>", ""));
+        list.add(new RichToPrime("h:selectOneMenu", "p:selectOneMenu"));
+        list.add(new RichToPrime("<s:selectItems",
+                "<f:selectItem itemLabel=\"Seciniz\" itemValue=\"\" /> <f:selectItems"));
+        list.add(new RichToPrime("</s:selectItems>", ""));
+
+        list.add(new RichToPrime("<s:convertEntity />", "itemLabel converter=\"MockBaseConverter\" "));
+        list.add(new RichToPrime("<s:convertEnum />", "itemLabel"));
+
+        list.add(new RichToPrime("oncomplete=\"applyMasks();\"", ""));
+
+        list.add(new RichToPrime("noSelectionLabel=\"#{messages['selectOneMenu.NoSelection']}\"", ""));
+        list.add(new RichToPrime("<a4j:support", "<p:ajax"));
+        list.add(new RichToPrime("event=\"onchange\"", "event=\"change\" "));
+        list.add(new RichToPrime("reRender", "update"));
+        list.add(new RichToPrime("ajaxSingle=\"true\"", "process=\"@this\""));
+        list.add(new RichToPrime("<h:outputLabel", "<p:outputLabel"));
+        list.add(new RichToPrime("<h:inputText", "<p:inputText"));
+        list.add(new RichToPrime("h:panelGrid", "p:panelGrid"));
+        list.add(new RichToPrime("h:selectBooleanCheckbox", "p:selectBooleanCheckbox"));
+        list.add(new RichToPrime("a4j:commandButton", "p:commandButton"));
+
+        list.add(new RichToPrime("<h:inputNumberSpinner", "<p:spinner"));
+
+        list.add(new RichToPrime("h:outputText", "p:outputLabel"));
+        list.add(new RichToPrime("</h:inputText>", "</p:inputText>"));
+        list.add(new RichToPrime("h:inputTextarea", "p:inputTextarea"));
+
+        list.add(new RichToPrime("<rich:calendar", "<p:calendar pattern=\"dd.MM.yyyy\" mask=\"true\" locale=\"tr\""));
+        list.add(new RichToPrime("popup=\"true\"", ""));
+
+        list.add(new RichToPrime("<s:conversationId />", ""));
+        list.add(new RichToPrime("<rich:extendedDataTable", "<p:dataTable emptyMessage=\"Kayıt Bulunamadı!\" "));
+        list.add(new RichToPrime("componentControl", "oncomplete=\"PF('Wv').show()\""));
+        list.add(new RichToPrime("<rich:modalPanel",
+                "<p:dialog closeOnEscape=\"true\" header=\"\" style=\"width: auto !important;\" widgetVar=\"Wv\" modal=\"true\"> <ui:include src=\"/pages/\" /> </p:dialog> <rich:modalPanel"));
+
+        list.add(new RichToPrime(".initHome}", ".create()}"));
+        list.add(new RichToPrime(".initHome()}", ".create()}"));
+
+        list.add(new RichToPrime("</a4j:support>", "</p:ajax>"));
+
+        list.add(new RichToPrime(".initializeDataModel()", ".search()"));
+
+        list.add(new RichToPrime("<ui:decorate template=\"/UC/common/PagingTemplate.xhtml\">",
+                "<ui:decorate template=\"/pages/pagingtemplate.xhtml\">"));
+
+        list.add(new RichToPrime("/UC/", "/pages/"));
+        list.add(new RichToPrime("<h:inputNumberSpinner", "<p:spinner"));
+        list.add(new RichToPrime("<s:convertDateTime pattern=\"dd.MM.yyyy\" />",
+                "<f:convertDateTime pattern=\"dd.MM.yyyy\" timeZone=\"Turkey\" locale=\"tr_TR\" />"));
+        list.add(new RichToPrime("Richfaces.hideModalPanel", "PF('Wv').hide()"));
+        list.add(new RichToPrime("Richfaces.showModalPanel", "PF('Wv').show()"));
+        list.add(new RichToPrime("enableManualInput=\"true\"", ""));
+        list.add(new RichToPrime("event=\"onhide\"", "event=\"close\""));
+        list.add(new RichToPrime("datePattern=\"dd.MM.yyyy\"", ""));
+        list.add(new RichToPrime("title=\"DÜZENLE\"", "value=\"DÜZENLE\""));
+        list.add(new RichToPrime("title=\"YENİ\"", "value=\"YENİ\""));
+        list.add(new RichToPrime("title=\"SİL\"", "value=\"SİL\""));
+        list.add(new RichToPrime("cellHeight=\"24\"", ""));
+        list.add(new RichToPrime("cellWidth=\"24px\"", ""));
+        list.add(new RichToPrime("cellHeight=\"22px\"", ""));
+        list.add(new RichToPrime("inputSize=\"32\"", ""));
+
+        list.add(new RichToPrime("noSelectionLabel=\"SEÇİNİZ\"", ""));
+
+        list.add(new RichToPrime("styleClass=\"input inputlowered\"", "lcase=\"\""));
+
+        list.add(new RichToPrime("alt=\"new_icon\"", "icon=\"ui-icon-add-circle-outline\""));
+        list.add(new RichToPrime("alt=\"download_icon\"", "icon=\"ui-icon-cloud-download\""));
+
+        list.add(new RichToPrime("<rich:simpleTogglePanel",
+                "<p:accordionPanel> <p:tab></p:tab> </p:accordionPanel> <rich:simpleTogglePanel"));
+        list.add(new RichToPrime("dateCalculateFunctions.dateToStr", "bWebDateFunctionsStatic.dateToStr"));
+        list.add(new RichToPrime("alt=\"phoneArea\"", "mask=\"(999) 999 99 99\""));
+        list.add(new RichToPrime("applyMasks();", ""));
+        list.add(new RichToPrime("renderButtons();", ""));
+        list.add(new RichToPrime("/rich:extendedDataTable", "/p:dataTable"));
+
+        list.add(new RichToPrime("oncomplete=\"\"", ""));
+        list.add(new RichToPrime("rich", "p"));
+        list.add(new RichToPrime("onclick=\"\"", ""));
+        list.add(new RichToPrime("#{beyazWebLicence", "#{bWebLicence"));
+        list.add(new RichToPrime(
+                "oncomplete=\"if(#{facesContext.maximumSeverity==null || facesContext.maximumSeverity.ordinal==0}) window.open('report.seam?cid=#{conversation.id}');\"",
+                "oncomplete=\"defaultRequestHandler(xhr, status, args);\""));
+        list.add(new RichToPrime("alt=\"new_icon\"", ""));
+        list.add(new RichToPrime("value=\"YENİ\"", "value=\"Yeni\" icon=\"ui-icon-add-circle-outline\""));
+
+    }
+
+    public RichToPrime(String rihc, String prime) {
+        super();
+        this.rihc = rihc;
+        this.prime = prime;
+    }
+
+    public static void add(RichToPrime richToPrime) {
+        list.add(richToPrime);
+    }
+
+    public String getRihc() {
+        return rihc;
+    }
+
+    public void setRihc(String rihc) {
+        this.rihc = rihc;
+    }
+
+    public String getPrime() {
+        return prime;
+    }
+
+    public void setPrime(String prime) {
+        this.prime = prime;
+    }
+}
---
+++ converter/src/XhtmlConverter.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class XhtmlConverter {
+    public static void main(String[] args) {
+        try {
+            System.err.println("converter started");
+            String filePath = "C:\\Users\\uurci\\Desktop\\xhtml";
+
+            //home name refactor bean
+            addExtra("paymentDocumentDefinition");
+
+            File folder = new File(filePath);
+            File[] listOfFiles = folder.listFiles();
+
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    Path path = Paths.get(filePath.concat(listOfFiles[i].getName()));
+                    Charset charset = StandardCharsets.UTF_8;
+
+                    String content = new String(Files.readAllBytes(path), charset);
+
+                    for (RichToPrime _prime : RichToPrime.list)
+                        content = content.replace(_prime.getRihc(), _prime.getPrime());
+
+                    Files.write(path, content.getBytes(charset));
+
+                }
+            }
+
+            System.err.println("converter stoped");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void addExtra(String entity) {
+        if (!entity.isEmpty()) {
+            RichToPrime r1 = new RichToPrime(entity + "Home.instance", entity + "Bean.editing");
+            RichToPrime r2 = new RichToPrime("!" + entity + "Home.managed", entity + "Bean.editing.recordId==null");
+            RichToPrime r3 = new RichToPrime(entity + "Home.managed", entity + "Bean.editing.recordId!=null");
+            RichToPrime r4 = new RichToPrime(entity + "Home", entity + "Bean");
+            RichToPrime r5 = new RichToPrime(entity + "List", entity + "Bean");
+
+            RichToPrime.add(r1);
+            RichToPrime.add(r2);
+            RichToPrime.add(r3);
+            RichToPrime.add(r4);
+            RichToPrime.add(r5);
+        }
+
+    }
+}
